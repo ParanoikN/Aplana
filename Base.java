7b2e75f2cc6b5e00f92ab4bec16e4347e6cbba4f
ParanoikNgit config --global user.name ParanoikNyesgit config --global user.name ParanoikN <tnsneek@yandex.ru>
2017-07-03 23:51:25 +0300

    3 commit

--- Base.java
+++ Base.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 /**
  * Created by Nikita Tretyakov on 24.06.2017.
  * @author Nikita Tretyakov
- * Калькулятор из первой лекции
+ * Калькулятор из Второй лекции
  */
 public class Base {
     public static void main(String[] args) {
-        System.out.println("Калькулятор");
+        System.out.println("Калькулятор сложение");
         System.out.println("Введите первое число");
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
---
+++ Lekciya3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * Created by Nikita Tretyakov on 28.06.2017.
+ * Калькулятор из лекции 3
+ */
+public class Lekciya3 {
+    public static void main(String[] args) {
+        System.out.println("Калькулятор полная версия");
+        System.out.println("Введите первое число");
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        System.out.println("Введите операцию");
+        String c = scanner.next();
+        System.out.println("Введите второе число");
+        double b = scanner.nextDouble();
+        if (c.equals("*")){
+            System.out.println("= " + (a * b));
+        }
+        if (c.equals("/")){
+            System.out.println("= " + (a / b));
+        }
+        if (c.equals("+")){
+            System.out.println("= " + (a + b));
+        }
+        if (c.equals("-")){
+            System.out.println("= " + (a - b));
+        }
+        scanner.close();
+    }
+}
