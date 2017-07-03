34b693ee3b9b1a36362c207700c0ed50aa5a8ba6
ParanoikNgit config --global user.name ParanoikNyesgit config --global user.name ParanoikN <tnsneek@yandex.ru>
2017-07-04 00:49:41 +0300

    commit

---
+++ MassivSlovLekciya3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Created by Tratyakov Nikita on 28.06.2017.
+ * Задача из лекции 3 про массивы
+ */
+public class MassivSlovLekciya3 {
+    public static void main(String[] args) {
+        System.out.print("Задайте размер массива: ");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        String[] Slova = new String[a];
+        int i = 0;
+        for (String element : Slova) {
+            System.out.print("Введите " + (i + 1) + "-й элемент массива: ");
+            Slova[i] = scanner.next();
+            i++;
+        }
+
+        for (){
+
+        }
+
+        for (String element : Slova)
+            System.out.print(element + " ");
+
+
+    }
+}
