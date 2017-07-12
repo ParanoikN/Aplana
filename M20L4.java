bbb426348686df8fc1494fe0d19ff94a2ac828dd
GannaChernyshova <anna.chernyshova91@gmail.com>
2017-07-13 02:11:34 +0300

    added comments to M20L4 class

--- M20L4.java
+++ M20L4.java
@@ -1,11 +1,22 @@
 /**
  * Created by Paranoik on 08.07.2017.
  */
+
+//опять-таки, названия классов, методов, переменных нужно более конкретно называть. Чтоб человек, который первый раз код видит
+//мог понять логику работы программы. вот M20L4, massiv(), ranomMassiv20L4 ни о чем не говорят) 
 public class M20L4 {
     public static void massiv() {
         int[] ranomMassiv20L4 = new int[20];
+        
+        //поиск элементов и генерацию массва нужно по разным методам разбивать. например:
+        //generateArrayOfRandomIntegerNumbers(int arraySize)
+        //getIndexOfMaxNegativeElement(int[] numbersArray)
+        //getIndexOfMinPositiveElement(int[] numbersArray)
+        //и необходимо добавить еще метод, который меняет найденные элементы местами:
+        //swapArrayElements(int[] array, int indexOfMax, int indexOfMin)
+        
         for (int i = 0; i < ranomMassiv20L4.length; i++) {
-            ranomMassiv20L4[i] = ((int) (Math.random() * 20) - 10);
+            ranomMassiv20L4[i] = ((int) (Math.random() * 20) - 10); //10 не получится
             System.out.println(ranomMassiv20L4[i]);
         }
         int maxNegativ = ranomMassiv20L4[0];
